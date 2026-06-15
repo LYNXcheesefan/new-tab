fd9afffa65ea72ec8903aca3566d6217a9c6e6e7
LYNXcheesefan <dzrivalsnoob@outlook.kr>
2026-06-15 13:04:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function updateClock() {
-    const now = new Date();
-    document.getElementById("clock").textContent =
-        now.toLocaleTimeString();
+  const now = new Date();
+
+  let h = now.getHours();
+  let m = now.getMinutes();
+  let s = now.getSeconds();
+
+  if (h < 10) h = "0" + h;
+  if (m < 10) m = "0" + m;
+  if (s < 10) s = "0" + s;
+
+  document.getElementById("clock").innerText =
+    `${h}:${m}:${s}`;
 }
 
 setInterval(updateClock, 1000);
